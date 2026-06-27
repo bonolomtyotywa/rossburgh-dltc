5d3a6ba4e567015ed9410fb2eec710187daba53a
bonolomtyotywa <bonolomtyotywa@gmail.com>
2026-06-27 18:19:42 +0200

    Added Firebase integration, queue, feedback, updates, admin pages

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,15 +3,17 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDNnjXTWQ5i-xDA4NRZ50v60iP6YNT9VMw",
+  authDomain: "rossburgh-dltc.firebaseapp.com",
+  projectId: "rossburgh-dltc",
+  storageBucket: "rossburgh-dltc.firebasestorage.app",
+  messagingSenderId: "41156791306",
+  appId: "1:41156791306:web:0fc4e477f7e447660163f3"
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
